ed0e0d98d07dcbfbb24a6ac312e8a61991e9a7fb
Daniel Bachhuber <danielbachhuber@gmail.com>
2012-08-05 17:42:13 -0500

    Rewrite main functions file into a simple class for simplicity's sake

--- functions.php
+++ functions.php
@@ -1,19 +1,23 @@
 <?php
 
-function register_my_menus() {
-  register_nav_menus(
-    array('navigation-menu' => __( 'Navigation Menu' ) )
-  );
-}
+class ONA12 {
 
-add_action( 'init', 'register_my_menus' );
+	function __construct() {
 
-add_action( 'init', 'create_post_types' );
+		add_action( 'init', array( $this, 'action_init' ) );
 
-function create_post_types() {
-	
-	register_post_type( 'frontpageslide',
-		array(
+	}
+
+	/**
+	 * Register menus, custom post types, etc.
+	 */
+	function action_init() {
+
+		register_nav_menus(
+			array('navigation-menu' => __( 'Navigation Menu' ) )
+		);
+
+		$args = array(
 			'label' => __( 'Front Page Slides' ),
 			'labels' => array(
 				'name' => __( 'Front Page Slides' ),
@@ -28,32 +32,30 @@ function create_post_types() {
 				'revisions'
 				),
 			'taxonomies' => array( 'slidetype' ),
-		)
-	);
+			);
+		register_post_type( 'frontpageslide', $args );
+
+		register_sidebar(array(
+			'name'           => 'Calendar',
+			'id'             => 'calendar-widgets',
+			'description'    => 'This fills in the ONA12 calendar.',
+			'before_widget'  => '<div id="%1$s" class="grid_2 widget %2$s">',
+			'after_widget'   => '</div>',
+			'before_title'   => '<h4>',
+			'after_title'    => '</h4>'
+		));
+
+		register_sidebar(array(
+			'name' => 'Sponsors (Top)',
+			'id'   => 'sponsors-top-widgets',
+			'description'   => 'This fills the top sponsor area.',
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h4>',
+			'after_title'   => '</h4>'
+		));
+	}
 }
 
-    if (function_exists('register_sidebar')) {
-    	register_sidebar(array(
-    		'name' => 'Calendar',
-    		'id'   => 'calendar-widgets',
-    		'description'   => 'This fills in the ONA12 calendar.',
-    		'before_widget' => '<div id="%1$s" class="grid_2 widget %2$s">',
-    		'after_widget'  => '</div>',
-    		'before_title'  => '<h4>',
-    		'after_title'   => '</h4>'
-    	));
-    }
-    
-        if (function_exists('register_sidebar')) {
-    	register_sidebar(array(
-    		'name' => 'Sponsors (Top)',
-    		'id'   => 'sponsors-top-widgets',
-    		'description'   => 'This fills the top sponsor area.',
-    		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-    		'after_widget'  => '</div>',
-    		'before_title'  => '<h4>',
-    		'after_title'   => '</h4>'
-    	));
-    }
-
-?>
+global $ona12;
+$ona12 = new ONA12();
