15be11b0acd39486e33b89938fd934619ac5e3b3
Daniel Bachhuber <danielbachhuber@gmail.com>
2012-09-16 11:41:42 -0500

    Sessions sidebar: Fix padding and add the date and location

--- sidebar-ona12_session.php
+++ sidebar-ona12_session.php
@@ -2,6 +2,7 @@
 
 	<section id="sidebar-logo">
 		<a href="<?php echo home_url(); ?>"><img src="<?php echo get_stylesheet_directory_uri();  ?>/img/ona12-300px-t.png" class="header-logo" /></a>
+		<h3>Sept. 20-22, Hyatt Regency San Francisco</h3>
 	</section>
 
 </aside>
