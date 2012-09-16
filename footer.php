dd17ed10a43d40a5b683ad0eebd0f821915899ca
Daniel Bachhuber <d@danielbachhuber.com>
2012-09-16 13:35:59 -0400

    Don't load unnecessary JS file

--- footer.php
+++ footer.php
@@ -96,9 +96,6 @@
 
 <!-- jQuery is called via the Wordpress-friendly way via functions.php -->
 
-<!-- this is where we put our custom functions -->
-<script src="<?php bloginfo('template_directory'); ?>/_/js/functions.js"></script>
-
 <!-- Asynchronous google analytics; this is the official snippet.
 	 Replace UA-XXXXXX-XX with your site's ID and uncomment to enable. -->
 	 
