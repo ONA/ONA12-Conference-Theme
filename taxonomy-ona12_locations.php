e0fe93c8f82db862adb0c9860288eaf7a0e16850
Daniel Bachhuber <danielbachhuber@gmail.com>
2012-09-20 13:02:24 -0500

    Use the session archive sidebar on the session archive pages

--- taxonomy-ona12_locations.php
+++ taxonomy-ona12_locations.php
@@ -2,7 +2,7 @@
 
 <div id="content-row" class="container_12">
 
-	<?php get_sidebar( 'ona12_session' ); ?>
+	<?php get_sidebar( 'ona12_session_archive' ); ?>
 
 	<?php get_template_part( 'loop', 'session_archive' ); ?>
 
