0c9801abb6a9b943d2125f1c0f9156e3ef70f557
Daniel Bachhuber <danielbachhuber@gmail.com>
2012-09-20 11:48:04 -0500

    Backport this function from core so the liveblog doesn't fatal

--- functions.php
+++ functions.php
@@ -129,3 +129,33 @@ class ONA12 {
 
 global $ona12;
 $ona12 = new ONA12();
+
+// Backport from core because the conference site hasn't upgraded :(
+if ( ! function_exists( 'wp_is_mobile' ) ) :
+/**
+ * Test if the current browser runs on a mobile device (smart phone, tablet, etc.)
+ *
+ * @return bool true|false
+ */
+function wp_is_mobile() {
+	static $is_mobile;
+
+	if ( isset($is_mobile) )
+		return $is_mobile;
+
+	if ( empty($_SERVER['HTTP_USER_AGENT']) ) {
+		$is_mobile = false;
+	} elseif ( strpos($_SERVER['HTTP_USER_AGENT'], 'Mobile') !== false // many mobile devices (all iPhone, iPad, etc.)
+		|| strpos($_SERVER['HTTP_USER_AGENT'], 'Android') !== false
+		|| strpos($_SERVER['HTTP_USER_AGENT'], 'Silk/') !== false
+		|| strpos($_SERVER['HTTP_USER_AGENT'], 'Kindle') !== false
+		|| strpos($_SERVER['HTTP_USER_AGENT'], 'BlackBerry') !== false
+		|| strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== false ) {
+			$is_mobile = true;
+	} else {
+		$is_mobile = false;
+	}
+
+	return $is_mobile;
+}
+endif;
